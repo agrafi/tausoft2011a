1cf351f05a6fab9e36fdaf7fa631714801486e28
asafbruner <asafbruner@af3a158a-b7d4-45a4-cee0-78ebc5f089f6>
2011-03-12 09:46:31 +0000

    Reinitial Rules

---
+++ src/helpers.h
@@ -0,0 +1,46 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+#include "misc.h"
+#include "stdio.h"
+
+// #define CREATE_AUTHENTICATION
+// #define AUTHENTICATE
+#define RULES_PREPROCESS
+
+#define MD5_OUTPUT_LENGTH_IN_BYTES    16
+#define SHA1_OUTPUT_LENGTH_IN_BYTES   20
+
+#define MAX_FIELD 255
+#define MAX_INPUT (MAX_FIELD + MAX_FIELD + 2)
+
+#define CMD_QUIT 0
+#define CMD_CONTINUE 1
+#define CMD_VALID 2
+
+#define MIN(x,y) ((x) < (y) ? (x) : (y))
+#define MAX(x,y) ((x) > (y) ? (x) : (y))
+
+enum Hashfunc {
+	MD5,
+	SHA1
+};
+
+
+typedef struct record_tag {
+	enum Hashfunc hash;
+	BasicHashFunctionPtr hashptr;
+	char username[MAX_FIELD];
+	char password[MAX_FIELD];
+	union {
+		unsigned char md5[MD5_OUTPUT_LENGTH_IN_BYTES];
+		unsigned char sha1[SHA1_OUTPUT_LENGTH_IN_BYTES];
+	} hashed_password;
+	unsigned int hashed_password_len;
+} record;
+
+void printHash(record* r, FILE* file);
+int fileexists(char* filename);
+int numOfLines(char* filename);
+int readLineFromUser(record* newuser);
+#endif
---
+++ src/rules.h
@@ -0,0 +1,60 @@
+/*
+ * rules.h
+ *
+ *  Created on: Mar 2, 2011
+ *      Author: a
+ */
+
+#ifndef RULES_H_
+#define RULES_H_
+
+enum CELLTYPE {
+	NUMBERS,
+	LETTERS,
+	ALPHANUMERIC,
+	CHARACTER,
+	LEX,
+	LEXCS
+};
+
+typedef struct passcell_struct {
+	enum CELLTYPE type;
+	char* data;
+	unsigned long range;
+} passcell;
+
+typedef struct passblock_struct {
+	enum CELLTYPE type;
+	char* data;
+	unsigned long range;
+	unsigned long numOfCells;
+	passcell* cells;
+} passblock;
+
+typedef struct passterm_struct {
+	unsigned long numOfBlocks;
+	unsigned long numOfPasswords;
+	char* term;
+	passblock* blocks;
+} passterm;
+
+typedef struct passgencontext_struct {
+	unsigned long numOfTerms;
+	unsigned long numOfPasswords;
+	passterm* terms;
+} passgencontext;
+
+typedef struct lexword_struct {
+	char* word;
+	char* wordlower;
+	char* playground;
+	unsigned long len;
+	unsigned long numOfLettersPermutaionsInWord;
+} lexword;
+typedef struct lexicon_struct {
+	unsigned long numOfWordsInLexicon;
+	unsigned long numOfLettersInLexicon;
+	unsigned long sumOfWordsPermutationsInLexicon;
+	lexword* words;
+} lexicon;
+#endif /* RULES_H_ */
