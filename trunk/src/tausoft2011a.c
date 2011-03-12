84c8027ce3e7c47223018e064410fa35b3215245
asafbruner <asafbruner@af3a158a-b7d4-45a4-cee0-78ebc5f089f6>
2011-03-12 10:57:05 +0000

    Multiterm working

---
+++ trunk/src/tausoft2011a.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : tausoft2011a.c
+ Author      : 
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#if SIMPLEMAIN
+int main(void) {
+	puts(""); /* prints  */
+	return EXIT_SUCCESS;
+}
+#endif
