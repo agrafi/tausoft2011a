9a1a36492d24fad1fcd132060643ea502890640e
asafbruner <asafbruner@af3a158a-b7d4-45a4-cee0-78ebc5f089f6>
2011-03-17 15:25:25 +0000

    Working DEHT query!

--- trunk/src/exhaustive_table_generator.c
+++ trunk/src/exhaustive_table_generator.c
@@ -15,21 +15,28 @@
 #ifdef EXHAUSTIVE_TABLE_GENERATOR
 int main(void)
 {
-	int i = 0;
+	int i = 0, len = 0, datalen = 0, keylen = 0;
 	DEHT* deht = create_empty_DEHT("deht", hashfun, validfun, "SHA1", 65536, ELEMENTS_PER_NODE, 8);
 	char* keybuf = calloc(1, 20);
 	char* databuf = calloc(1, 30);
-	snprintf(databuf, sizeof(databuf), "%s", "123456");
-	hexa2binary("7c4a8d09ca3762af61e59520943dc26494f8941b", keybuf, sizeof(keybuf));
-	add_DEHT(deht, keybuf, sizeof(keybuf), databuf, strlen(databuf));
+	datalen = hexa2binary("123456", databuf, 30);
+	keylen = hexa2binary("7c4a8d09ca3762af61e59520943dc26494f8941b", keybuf, 20);
+	add_DEHT(deht, keybuf, keylen, databuf, datalen);
 
-	for (i=0; i<10; i++)
+	for (i=0; i<11; i++)
 	{
-		snprintf(databuf, sizeof(databuf), "%s", "6543210");
-		hexa2binary("dd5fef9c1c1da1394d6d34b248c51be2ad740840", keybuf, sizeof(keybuf));
-		add_DEHT(deht, keybuf, sizeof(keybuf), databuf, strlen(databuf));
+		datalen = hexa2binary("65432100", databuf, 30);
+		keylen = hexa2binary("dd5fef9c1c1da1394d6d34b248c51be2ad740840", keybuf, 20);
+		add_DEHT(deht, keybuf, keylen, databuf, datalen);
 	}
 	write_DEHT_pointers_table(deht);
+	datalen = hexa2binary("123456", databuf, 30);
+	keylen = hexa2binary("7c4a8d09ca3762af61e59520943dc26494f8941b", keybuf, 20);
+	memset(databuf, 0, 30);
+	len = query_DEHT(deht, keybuf, 20, databuf, 30);
+	memset(keybuf, 0 , 20);
+	binary2hexa(databuf, len, keybuf, 20);
+	printf("data is %s\n", keybuf);
 	return EXIT_SUCCESS;
 }
 #endif
